f3cda2651d19d1168d65be7d170a6e1b5e19a976
slonofob <kostin.a.p@gmail.com>
2018-09-12 02:46:39 +0500

    Revert "Функции 1.8"
    This reverts commit 241ad972e7b8cfbd6dc8ce7317a4362693c2a928.

--- homebox/src/main/java/ru/stqa/pft/homebox/MyFirstProgram.java
+++ homebox/src/main/java/ru/stqa/pft/homebox/MyFirstProgram.java
@@ -7,26 +7,22 @@ class MyFirstProgram {
         hello("World");
         hello("User");
         hello("Aleksander");
-
-        Square s = new Square();
-        s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
-
-        Rectangle r = new Rectangle();
-        r.b = 10;
-        r.a = 5;
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a +" и "+ b + " = " + area(a,b));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello  " + somebody + "!");
     }
 
-    public static double area(Square s) {
-        return s.l * s.l;
+    public static double area(double len) {
+        return len * len;
     }
+public  static double area (double a, double b){
+        return a*b;
 
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
+}
 }
