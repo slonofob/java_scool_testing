2e7d2ed4e8277e9dd1b6d395cafba0f53ba548ec
slonofob <kostin.a.p@gmail.com>
2018-09-12 23:40:39 +0500

    Задание 2 пункт 4
    Содержит изменения кода для выполнения  пункта:
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.

--- homebox/src/main/java/ru/stqa/pft/homebox/CalculationDistance.java
+++ homebox/src/main/java/ru/stqa/pft/homebox/CalculationDistance.java
@@ -3,21 +3,25 @@ package ru.stqa.pft.homebox;
 public class CalculationDistance {
     public static void main(String[] args) {
 
-        Point p1 = new Point();
-        Point p2 = new Point();
-        //создание объекта
+        Point p1 = new Point(10,15);
+        Point p2 = new Point(15,20);
+        /*
         p1.x = 10.0;
         p1.y = 10.0;
         p2.x = 0;
         p2.y = 0;
-        //заведение переменных
+        */
 
-        System.out.println("Растояние между точками" + "(" + p1.x + "," + p1.y + ")" + " и (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
-        //вывод на экран
+        //System.out.println("Растояние между точками" + "(" + p1.x + "," + p1.y + ")" + " и (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+        //вывод на экран функции
+
+        System.out.println("Растояние между точками" + "(" + p1.x + "," + p1.y + ")" + " и (" + p2.x + "," + p2.y + ") = " + p1.distance(p2));
+        //вывод на экран метода
     }
 
+  /*использование функции
     public static double distance(Point p1, Point p2) {
         return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)));
-//создание функции с вычислением расстояния
     }
+ */
 }
--- homebox/src/main/java/ru/stqa/pft/homebox/Point.java
+++ homebox/src/main/java/ru/stqa/pft/homebox/Point.java
@@ -3,4 +3,12 @@ package ru.stqa.pft.homebox;
 public class Point {
     double x;
     double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point to) {
+        return Math.sqrt(((this.x - to.x) * (this.x - to.x) + (this.y - to.y) * (this.y - to.y)));
+    } //добавление метода
 }
