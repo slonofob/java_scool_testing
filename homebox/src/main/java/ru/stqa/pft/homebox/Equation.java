a0b49b0d237591c5cddf51c5439e40398ba535f4
slonofob <kostin.a.p@gmail.com>
2018-11-06 23:12:33 +0500

    Лекция 3.1 Проверка условий при помощи конструкции if-then-else

---
+++ homebox/src/main/java/ru/stqa/pft/homebox/Equation.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.homebox;
+
+public class Equation {
+    private final double a;
+    private final double b;
+    private final double c;
+    private int n;
+
+    public Equation(double a, double b, double c) {
+
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        double d = b * b - 4 * a * c;
+
+        if (d > 0) {
+            n = 2;
+        } else if (d == 0) {
+            n = 1;
+        } else {
+            n = 0;
+        }
+
+    }
+
+    public int rootNumber() {
+        return n;
+    }
+
+}
